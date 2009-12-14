8f8e077ceab7d9441697ae2c57bd35fc6a04dc6f
Frédéric Péters <fpeters@0d.be>
2009-12-14 22:45:30 +0100

    Remove shebang lines from Python plain modules

--- channel.py
+++ channel.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
-#
 # This file is part of jack_mixer
 #
 # Copyright (C) 2006 Nedko Arnaudov <nedko@arnaudov.name>
-#  
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; version 2 of the License

870bb8b84163280e80e61d52bb2172b61f90fe55
Christopher Arndt <chris@chrisarndt.de>
2021-02-21 20:16:34 +0100

    Simplify Cython module C source generation for source dist
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- src/meson_dist_cython.py
+++ src/meson_dist_cython.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+import shutil
 import sys
-from os import environ, getcwd
-from os.path import exists, join, splitext
+from os import chdir, environ, getcwd
+from os.path import join
 
 from subprocess import run
 
@@ -12,8 +13,8 @@ dist_root = environ.get("MESON_DIST_ROOT")
 source_root = environ.get("MESON_SOURCE_ROOT")
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-v', '--verbose', action="store_true", help="Be more verbose.")
-ap.add_argument('pyx', nargs="*", help="Cython source files (*.pyx).")
+ap.add_argument("-v", "--verbose", action="store_true", help="Be more verbose.")
+ap.add_argument("mod_source", nargs="*", help="Cython module C source target(s) (*.c).")
 args = ap.parse_args()
 
 if args.verbose:
@@ -23,26 +24,22 @@ if args.verbose:
     print("source root:", source_root)
     print("sys.argv:", sys.argv)
 
-for pyx in args.pyx:
-    src = join(source_root, 'src', pyx)
-    dst = join(dist_root, "src", splitext(pyx)[0] + '.c')
+for mod in args.mod_source:
+    target = join("src", mod)
+    dst = join(dist_root, "src", mod)
 
-    if exists(src):
-        print("Cythonizing source file '%s'..." % src)
-        print("Output file: '%s'" % dst)
-        cmd = ["cython"]
+    print("Updating Cython module C source '%s'..." % target)
+    cmd = ["ninja"]
 
-        if args.verbose:
-            cmd += ["-v"]
+    if args.verbose:
+        cmd += ["-v"]
 
-        cmd += ["-o", dst, src]
-        try:
-            proc = run(cmd)
+    cmd += [target]
 
-            if proc.returncode != 0:
-                sys.exit("cython returned non-zero (%i) for '%s'." %
-                         (proc.returncode, pyx))
-        except FileNotFoundError:
-            sys.exit("The 'cython' program was not found but is required to build a "
-                     "distribution.\nPlease install Cython from: "
-                     "https://pypi.org/project/Cython/")
+    chdir(build_root)
+    proc = run(cmd)
+
+    if proc.returncode != 0:
+        sys.exit("'ninja' returned non-zero (%i) for target '%s'." % (proc.returncode, target))
+
+    shutil.copy(target, dst)
